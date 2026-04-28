f4a913dac01af14559cec8d78e7aeeaab1e9fab5
mollika123 <aktermollika49@gmail.com>
2026-04-28 09:55:22 +0200

    proxy implement

--- src/components/homepage/news/RightSideBar.jsx
+++ src/components/homepage/news/RightSideBar.jsx
@@ -1,14 +1,30 @@
+"use client"
+
+import { authClient } from "@/lib/auth-client";
 import { FaGithub, FaGoogle } from "react-icons/fa";
 
 
 const RightSideBar = () => {
+
+  const handleGoogleSignin =async () => {
+      const data = await authClient.signIn.social({
+    provider: "google",
+      });
+    console.log("data",data);
+  }
+  const handleGithubSignin =async () => {
+      const data = await authClient.signIn.social({
+    provider: "github",
+      });
+    console.log("data",data);
+  }
   return (
     <div>
       <h2 className="text-xl font-bold mb-4">Login With</h2>
       <div className="flex flex-col gap-2">
-        <button className="btn border-blue-500 text-blue-500">
+        <button  onClick={handleGoogleSignin} className="btn border-blue-500 text-blue-500">
           <FaGoogle />Login with google</button>
-        <button className="btn"><FaGithub />Login with github</button>
+        <button onClick={ handleGithubSignin} className="btn"><FaGithub />Login with github</button>
       </div>
     </div>
   );
---
+++ src/proxy.js
@@ -0,0 +1,29 @@
+import dns from "node:dns"
+dns.setServers(['8.8.8.8', '8.8.4.4']);
+
+import { NextResponse } from 'next/server'
+import { authClient } from './lib/auth-client';
+import { auth } from './lib/auth';
+import { headers } from 'next/headers';
+ 
+// This function can be marked `async` if using `await` inside
+export async function proxy(request) {
+
+  const session = await auth.api.getSession({
+        headers: await headers()
+  })
+  console.log("session",session);
+  // const isLoggedIn = false;
+  if (session) {
+  return NextResponse.next()  
+  }
+  return NextResponse.redirect(new URL('/login', request.url))
+  ;
+  // return NextResponse.redirect(new URL('/login', request.url));
+}
+ 
+
+ 
+export const config = {
+  matcher: ['/career',"/news/:path*"],
+}
